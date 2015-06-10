e121fa001597626324448146e44117e65183704c
David Esposito <david@charbox.co>
2015-06-10 08:48:26 -0400

    Finishing update. Adding exit command after client tasks run.

--- src/main/java/co/charbox/client/controller/ControllerMain.java
+++ src/main/java/co/charbox/client/controller/ControllerMain.java
@@ -1,5 +1,6 @@
 package co.charbox.client.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import co.charbox.client.ClientMain;
 import co.charbox.client.hb.HeartbeatMain;
 import co.charbox.client.ping.PingMain;
@@ -7,6 +8,7 @@ import co.charbox.client.sst.SstMain;
 import co.charbox.client.sst.SstSelfServer;
 import co.charbox.client.upgrade.UpgradeMain;
 
+@Slf4j
 public class ControllerMain {
 
 	public static void main(String[] args) throws Exception {
@@ -14,23 +16,33 @@ public class ControllerMain {
 			throw new IllegalArgumentException("Expected single argument. [client, sst, ping, hb]");
 		}
 		if (args[0].equals("client")) {
-			System.out.println("Starting Client");
+			log.info("Starting Client");
 			ClientMain.main(args);
 		} else if (args[0].equals("sst")) {
-			System.out.println("Starting SST");
+			log.info("Starting SST");
 			SstMain.main(args);
+			log.info("SST Complete, exiting client.");
+			System.exit(0);
 		} else if (args[0].equals("self-sst")) {
-			System.out.println("Starting Self-SST");
+			log.info("Starting Self-SST");
 			SstSelfServer.main(args);
+			log.info("Self-SST Complete, exiting client.");
+			System.exit(0);
 		} else if (args[0].equals("ping")) {
-			System.out.println("Starting Ping");
+			log.info("Starting Ping");
 			PingMain.main(args);
+			log.info("Ping Complete, exiting client.");
+			System.exit(0);
 		} else if (args[0].equals("hb")) {
-			System.out.println("Starting Heartbeat");
+			log.info("Starting Heartbeat");
 			HeartbeatMain.main(args);
+			log.info("Heartbeat Complete, exiting client.");
+			System.exit(0);
 		} else if (args[0].equals("upgrade")) {
-			System.out.println("Starting Upgrade");
+			log.info("Starting Upgrade");
 			UpgradeMain.main(args);
+			log.info("Upgrade Complete, exiting client.");
+			System.exit(0);
 		} else {
 			throw new IllegalArgumentException("Expected valid command. [client, sst, ping, hb]");
 		}
