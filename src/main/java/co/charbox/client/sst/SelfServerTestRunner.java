75331296b7d7ff429d91d819070b6e5ca90a4ace
David Esposito <david@charbox.co>
2015-07-11 07:44:39 -0400

    'Moving sst-common files to charboX package because of spring.'

--- src/main/java/co/charbox/client/sst/SelfServerTestRunner.java
+++ src/main/java/co/charbox/client/sst/SelfServerTestRunner.java
@@ -12,18 +12,18 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.joda.time.DateTime;
 
-import co.charbot.sst.InvalidDeviceTokenException;
-import co.charbot.sst.SSTProperties;
-import co.charbot.sst.results.SstResultsHandler;
-import co.charbot.sst.utils.DataReceiver;
-import co.charbot.sst.utils.DataSender;
-import co.charbot.sst.utils.MyIOHAndler;
 import co.charbox.core.utils.SpeedUtils;
 import co.charbox.domain.model.DeviceModel;
 import co.charbox.domain.model.SstResultsModel;
 import co.charbox.domain.model.mm.ConnectionInfoModel;
 import co.charbox.domain.model.mm.ConnectionModel;
 import co.charbox.domain.model.mm.SimpleLocationModel;
+import co.charbox.sst.InvalidDeviceTokenException;
+import co.charbox.sst.SSTProperties;
+import co.charbox.sst.results.SstResultsHandler;
+import co.charbox.sst.utils.DataReceiver;
+import co.charbox.sst.utils.DataSender;
+import co.charbox.sst.utils.MyIOHAndler;
 
 @Slf4j
 @Builder
